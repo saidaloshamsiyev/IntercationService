b0c2851c03139c8766c6d6ef9bba1582f47a050f
said4ik <muxammadaminartikov@gmail.com>
2024-09-27 11:43:14 +0500

    Initial commit

---
+++ src/main/java/metube/com/intercationservice/IntercationServiceApplication.java
@@ -0,0 +1,13 @@
+package metube.com.intercationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntercationServiceApplication.class, args);
+    }
+
+}
