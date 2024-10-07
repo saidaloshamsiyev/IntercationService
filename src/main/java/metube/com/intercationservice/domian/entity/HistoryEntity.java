0e12461fed2ee6987914e7aeaad25aa25a95c0c2
BehzodFarkhodov <behzodfarhodov13@gmail.com>
2024-10-07 14:26:04 +0500

    fdvfvfvf

--- src/main/java/metube/com/intercationservice/domian/entity/HistoryEntity.java
+++ src/main/java/metube/com/intercationservice/domian/entity/HistoryEntity.java
@@ -1,16 +1,12 @@
 package metube.com.intercationservice.domian.entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
-
 @Entity
 @Table(name = "history")
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
