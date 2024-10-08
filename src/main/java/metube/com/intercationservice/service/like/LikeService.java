895f5998a2f3cee5681291ace4a67db67820e4dd
soh1b003 <sohibjinnorboboyev@gmail.com>
2024-10-08 16:19:44 +0500

    controller commit and history

--- src/main/java/metube/com/intercationservice/service/like/LikeService.java
+++ src/main/java/metube/com/intercationservice/service/like/LikeService.java
@@ -1,5 +1,15 @@
 package metube.com.intercationservice.service.like;
 
-public interface LikeService {
+import metube.com.intercationservice.domian.dto.request.LIkeReq;
+import metube.com.intercationservice.domian.dto.response.LikeRes;
+import metube.com.intercationservice.domian.entity.LikeEntity;
 
+import java.util.List;
+import java.util.UUID;
+
+public interface LikeService {
+    LikeRes create(LIkeReq lIkeReq);
+    LikeEntity findById(UUID id);
+    List<LikeRes> findAll();
+    void delete(UUID id);
 }
