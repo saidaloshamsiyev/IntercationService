1ac9fe7face5b3e8753c07181906a8a95732a8ec
BehzodFarkhodov <behzodfarhodov13@gmail.com>
2024-10-16 16:01:16 +0500

    test committ

--- src/main/java/metube/com/intercationservice/filter/CustomFilter.java
+++ src/main/java/metube/com/intercationservice/filter/CustomFilter.java
@@ -9,21 +9,22 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.List;
 
-public class CustomFilter extends OncePerRequestFilter {
+//public class CustomFilter extends OncePerRequestFilter {
+//
+//    @Override
+//    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws ServletException, IOException {
+//        String username = request.getParameter("X-Username");
+//        String roles = request.getParameter("X-Role");
+//
+//        if (username == null || roles == null) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
+//        UsernamePasswordAuthenticationToken authenticationToken =
+//                new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority("ROLE_" + roles)));
+//        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//
+//        filterChain.doFilter(request, response);
+//    }
+//}
 
-    @Override
-    protected void doFilterInternal(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws ServletException, IOException {
-        String username = request.getParameter("X-Username");
-        String roles = request.getParameter("X-Role");
-
-        if (username == null || roles == null) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(username, null, List.of(new SimpleGrantedAuthority("ROLE_" + roles)));
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-        filterChain.doFilter(request, response);
-    }
-}
