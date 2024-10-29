18343c2c4307c529e123e82f4163e046f1e9f87f
soh1b003 <sohibjinnorboboyev@gmail.com>
2024-10-29 21:14:55 +0500

    urls update

--- src/main/java/metube/com/intercationservice/controller/HistoryController.java
+++ src/main/java/metube/com/intercationservice/controller/HistoryController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/history")
+@RequestMapping("/api/history")
 @RequiredArgsConstructor
 public class HistoryController {
     private final HistoryService historyService;
